9dd1da57f5d153db73d941c9590694b6559f51cb
Darren <darren@odden.io>
2023-12-21 16:24:45 -0800

    About page put together

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-elucidator-700 dark:text-dark-repulser-400 px-4 lg:px-0">this is about page</h1>
+  <div class="py-5 px-4">
+    <h1 class="text-elucidator-700 dark:text-dark-repulser-400 px-4 lg:px-0">The Charismatic Codefauna - Darren</h1>
+
+    <div class="mt-5 mb-5 text-elucidator-500 dark:text-elucidator-50">
+      <div class="article-body md-5">
+        <p>Welcome to Darren's digital space, a portal into his multifaceted world where curiosity and creativity converge. This site is Darren's canvas, capturing the breadth of his interests and learnings in areas like programming, design, marketing, culinary arts, and beyond.</p><p>Meet Darren: a software engineer with a designer's heart and an explorer's mind. His journey isn't one of a specialist in a single niche, but rather a mosaic of diverse skills and experiences. Darren's quest for knowledge is insatiable. He dives deep into subjects that catch his fancy, be it typeface design, marketing strategy, or anything in-between.</p>
+        <p>Darren's professional background is as rich and varied as his interests. He has honed his skills in Fortune 500 computer companies, adeptly tackling challenges in hardware and software. From repairing broken systems to managing large-scale software projects, Darren's expertise spans across multiple domains. His collaborations with global teams to launch software for cutting-edge hardware and his knack for navigating the complexities of the internet security world exemplify his adaptability and breadth of knowledge.</p>
+        <p>But there's more to Darren than just his technical acumen. His artistic side, inherited from parents deeply rooted in art and music, finds expression in illustration, photography, and videography. Inspired by the likes of Frank Lloyd Wright and the Bauhaus movement, Darren's love for architecture and design adds another layer to his multifaceted personality.</p>
+        <p>Darren's approach to life and work is a blend of simplicity and complexity. He champions the 'keep it simple' philosophy, yet his life story is anything but simple. His path has been winding and unpredictable, rich with experiences and learning.</p>
+        <p>Darren is a unique amalgam of traits – a 'simple complex charismatic codefauna', constantly evolving and adapting, always on a journey of discovery and innovation. Join him here, where every page is a new chapter in his ever-unfolding story.</p>
+
+      </div>
+    </div>
+  </div>
 </template>
